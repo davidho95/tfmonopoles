b2d0379d5f280a90836f5d06f076ce862873a70a
davidho95 <davidho95@gmail.com>
2020-11-23 13:51:01 +0000

    Added radial Georgi-Glashow theory

--- tfmonopoles/theories/__init__.py
+++ tfmonopoles/theories/__init__.py
@@ -1,3 +1,4 @@
 from tfmonopoles.theories.GeorgiGlashowSu2Theory import GeorgiGlashowSu2Theory
 from tfmonopoles.theories.GeorgiGlashowSu2TheoryUnitary import GeorgiGlashowSu2TheoryUnitary
 from tfmonopoles.theories.ElectroweakTheoryUnitary import ElectroweakTheoryUnitary
+from tfmonopoles.theories.GeorgiGlashowRadialTheory import GeorgiGlashowRadialTheory

df2dc60605ddf80481d21f60436e7e59f9afb6ac
davidho95 <davidho95@gmail.com>
2021-01-24 18:24:44 +0000

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='tfmonopoles',
-	version='1.0',
-	author='David Ho',
-	packages=find_packages())
+    version='1.0',
+    author='David Ho',
+    install_requires=[
+        'numpy',
+        'tensorflow>=2.2.0'
+    ],
+    packages=find_packages())
